c4f88c270bb4f6ebce67978ec85859ff2ed42cb0
taemin19 <daniel.thebault19@gmail.com>
2019-01-01 21:04:55 +0100

    test: add unit tests for entities

---
+++ tests/AppBundle/Entity/UserTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\AppBundle\Entity;
+
+use AppBundle\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * This test checks that the methods
+     * are correctly returned / called.
+     */
+    public function testMethod()
+    {
+        $user = new User();
+
+        $this->assertNull($user->getId());
+
+        $user->setUsername('user');
+        $this->assertSame('user', $user->getUsername());
+
+        $this->assertNull($user->getSalt());
+
+        $user->setPassword('password');
+        $this->assertSame('password', $user->getPassword());
+
+        $user->setEmail('user@email.com');
+        $this->assertSame('user@email.com', $user->getEmail());
+
+        $this->assertSame(['ROLE_USER'], $user->getRoles());
+    }
+}
