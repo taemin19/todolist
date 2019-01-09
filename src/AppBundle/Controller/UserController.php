48f69def44117aab9fdb014e6be56bb03efebb18
taemin19 <daniel.thebault19@gmail.com>
2019-01-09 16:33:35 +0100

    refactor: use dependency injection in controllers
    - Controllers don't extend a base controller class
    - Inject directly the dependencies via a constructor dependency injection or "action" injection.

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -4,66 +4,138 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\User;
 use AppBundle\Form\UserType;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class UserController extends Controller
+class UserController
 {
     /**
+     * @var \Twig_Environment
+     */
+    private $twig;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * UserController constructor.
+     *
+     * @param \Twig_Environment      $twig
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(\Twig_Environment $twig, EntityManagerInterface $entityManager)
+    {
+        $this->twig = $twig;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
      * @Route("/users", name="user_list")
+     *
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     *
+     * @return Response
      */
     public function listAction()
     {
-        return $this->render('user/list.html.twig', ['users' => $this->getDoctrine()->getRepository('AppBundle:User')->findAll()]);
+        return new Response(
+            $this->twig->render('user/list.html.twig', [
+                'users' => $this->entityManager->getRepository('AppBundle:User')->findAll(),
+            ])
+        );
     }
 
     /**
      * @Route("/users/create", name="user_create")
+     *
+     * @param FormFactoryInterface         $formFactory
+     * @param Request                      $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param FlashBagInterface            $flashBag
+     * @param RouterInterface              $router
+     *
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     *
+     * @return RedirectResponse|Response
      */
-    public function createAction(Request $request)
+    public function createAction(FormFactoryInterface $formFactory, Request $request, UserPasswordEncoderInterface $passwordEncoder, FlashBagInterface $flashBag, RouterInterface $router)
     {
         $user = new User();
-        $form = $this->createForm(UserType::class, $user);
-
+        $form = $formFactory->create(UserType::class, $user);
         $form->handleRequest($request);
 
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPassword());
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $passwordEncoder->encodePassword($user, $user->getPassword());
             $user->setPassword($password);
 
-            $em->persist($user);
-            $em->flush();
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
 
-            $this->addFlash('success', "L'utilisateur a bien été ajouté.");
+            $flashBag->add('success', "L'utilisateur a bien été ajouté.");
 
-            return $this->redirectToRoute('user_list');
+            return new RedirectResponse(
+                $router->generate('user_list')
+            );
         }
 
-        return $this->render('user/create.html.twig', ['form' => $form->createView()]);
+        return new Response(
+            $this->twig->render('user/create.html.twig', [
+                'form' => $form->createView(),
+            ])
+        );
     }
 
     /**
      * @Route("/users/{id}/edit", name="user_edit")
+     *
+     * @param FormFactoryInterface         $formFactory
+     * @param User                         $user
+     * @param Request                      $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param FlashBagInterface            $flashBag
+     * @param RouterInterface              $router
+     *
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     *
+     * @return RedirectResponse|Response
      */
-    public function editAction(User $user, Request $request)
+    public function editAction(FormFactoryInterface $formFactory, User $user, Request $request, UserPasswordEncoderInterface $passwordEncoder, FlashBagInterface $flashBag, RouterInterface $router)
     {
-        $form = $this->createForm(UserType::class, $user);
-
+        $form = $formFactory->create(UserType::class, $user);
         $form->handleRequest($request);
 
-        if ($form->isValid()) {
-            $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPassword());
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $passwordEncoder->encodePassword($user, $user->getPassword());
             $user->setPassword($password);
 
-            $this->getDoctrine()->getManager()->flush();
+            $this->entityManager->flush();
 
-            $this->addFlash('success', "L'utilisateur a bien été modifié");
+            $flashBag->add('success', "L'utilisateur a bien été modifié");
 
-            return $this->redirectToRoute('user_list');
+            return new RedirectResponse(
+                $router->generate('user_list')
+            );
         }
 
-        return $this->render('user/edit.html.twig', ['form' => $form->createView(), 'user' => $user]);
+        return new Response(
+            $this->twig->render('user/edit.html.twig', [
+                'form' => $form->createView(), 'user' => $user,
+            ])
+        );
     }
 }
