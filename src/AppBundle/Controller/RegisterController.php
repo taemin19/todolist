adcee280aafaa0e2590ce1c3cd3f484f6b166679
taemin19 <daniel.thebault19@gmail.com>
2019-02-04 17:10:46 +0100

    feat: create a controller to register a user

---
+++ src/AppBundle/Controller/RegisterController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\User;
+use AppBundle\Form\UserRegisterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+/**
+ * Controller used to register a user account.
+ */
+class RegisterController
+{
+    /**
+     * @var \Twig_Environment
+     */
+    private $twig;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * RegisterController constructor.
+     *
+     * @param \Twig_Environment      $twig
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(\Twig_Environment $twig, EntityManagerInterface $entityManager)
+    {
+        $this->twig = $twig;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/register", methods={"GET", "POST"}, name="user_register")
+     *
+     * @param FormFactoryInterface         $formFactory
+     * @param Request                      $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param FlashBagInterface            $flashBag
+     * @param RouterInterface              $router
+     *
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     *
+     * @return Response
+     */
+    public function registerAction(FormFactoryInterface $formFactory, Request $request, UserPasswordEncoderInterface $passwordEncoder, FlashBagInterface $flashBag, RouterInterface $router): Response
+    {
+        $user = new User();
+
+        $form = $formFactory->create(UserRegisterType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $passwordEncoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
+            $flashBag->add('success', sprintf('Bienvenue %s , votre compte a bien été crée.', $user->getUsername()));
+
+            return new RedirectResponse(
+                $router->generate('homepage')
+            );
+        }
+
+        return new Response(
+            $this->twig->render('register/register.html.twig', [
+                'form' => $form->createView(),
+            ])
+        );
+    }
+}
