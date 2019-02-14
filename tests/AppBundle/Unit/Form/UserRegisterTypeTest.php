0a9bed114f86ee4691bbbeae4b5b4853475ea1b0
taemin19 <daniel.thebault19@gmail.com>
2019-02-14 16:38:16 +0100

    test: add unit tests for UserRegisterType

---
+++ tests/AppBundle/Unit/Form/UserRegisterTypeTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\AppBundle\Unit\Form;
+
+use AppBundle\Entity\User;
+use AppBundle\Form\UserRegisterType;
+use Symfony\Component\Form\Extension\Validator\ValidatorExtension;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\Test\TypeTestCase;
+use Symfony\Component\Validator\ConstraintViolationList;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class UserRegisterTypeTest extends TypeTestCase
+{
+    private $validator;
+
+    /**
+     * This helper method mocks the ValidatorExtension.
+     *
+     *
+     * @throws \ReflectionException
+     *
+     * @return array
+     */
+    protected function getExtensions()
+    {
+        $this->validator = $this->createMock(ValidatorInterface::class);
+
+        $this->validator
+            ->method('validate')
+            ->will($this->returnValue(new ConstraintViolationList()));
+        $this->validator
+            ->method('getMetadataFor')
+            ->will($this->returnValue(new ClassMetadata(Form::class)));
+
+        return [
+            new ValidatorExtension($this->validator),
+        ];
+    }
+
+    /**
+     * This test checks that the Form and FormView
+     * are correctly generated.
+     */
+    public function testBuildForm()
+    {
+        $formData = [
+            'username' => 'user',
+            'password' => 'password',
+            'email' => 'user@email.com',
+        ];
+
+        $object = new User();
+        $form = $this->factory->create(UserRegisterType::class, $object);
+
+        $form->submit($formData);
+        $this->assertTrue($form->isSynchronized());
+
+        $view = $form->createView();
+        $children = $view->children;
+
+        foreach (array_keys($formData) as $key) {
+            $this->assertArrayHasKey($key, $children);
+        }
+    }
+}
