0c0209c0856a7c9ddae21924a1cdabd74e492c37
taemin19 <daniel.thebault19@gmail.com>
2019-02-15 01:58:37 +0100

    test: add unit tests for TaskVoter

---
+++ tests/AppBundle/Unit/Security/TaskVoterTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Tests\AppBundle\Unit\Security;
+
+use AppBundle\Entity\Task;
+use AppBundle\Entity\User;
+use AppBundle\Security\TaskVoter;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Security\Core\Authentication\Token\AnonymousToken;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class TaskVoterTest extends TestCase
+{
+    /**
+     * @param string $attribute
+     * @param Task   $task
+     * @param User   $user
+     * @param $expectedVote
+     *
+     * @dataProvider provideCases
+     */
+    public function testVote(string $attribute, Task $task, ?User $user, $expectedVote)
+    {
+        $voter = new TaskVoter();
+
+        $token = new AnonymousToken('secret', 'anonymous');
+        if ($user) {
+            if ($user->getId() === $task->getUser()->getId()) {
+                $token = new UsernamePasswordToken(
+                    $task->getUser(), 'credentials', 'memory'
+                );
+            } else {
+                $token = new UsernamePasswordToken(
+                    $user, 'credentials', 'memory'
+                );
+            }
+        }
+
+        $this->assertSame(
+            $expectedVote,
+            $voter->vote($token, $task, [$attribute])
+        );
+    }
+
+    /**
+     * @throws \ReflectionException
+     *
+     * @return array
+     */
+    public function provideCases()
+    {
+        return [
+            'anonymous cannot edit' => ['edit', $this->createTask(1), null, Voter::ACCESS_DENIED],
+            'anonymous cannot delete' => ['delete', $this->createTask(1), null, Voter::ACCESS_DENIED],
+            'non-owner cannot edit' => ['edit', $this->createTask(1), $this->createUser(2), Voter::ACCESS_DENIED],
+            'non-owner cannot delete' => ['delete', $this->createTask(1), $this->createUser(2), Voter::ACCESS_DENIED],
+            'owner can edit' => ['edit', $this->createTask(1), $this->createUser(1), Voter::ACCESS_GRANTED],
+            'owner can delete' => ['delete', $this->createTask(1), $this->createUser(1), Voter::ACCESS_GRANTED],
+        ];
+    }
+
+    /**
+     * @param int $id
+     *
+     * @throws \ReflectionException
+     *
+     * @return MockObject|User
+     */
+    private function createUser(int $id): MockObject
+    {
+        $user = $this->createMock(User::class);
+        $user->method('getId')->willReturn($id);
+
+        return $user;
+    }
+
+    /**
+     * @param int $userId
+     *
+     * @throws \ReflectionException
+     *
+     * @return Task
+     */
+    private function createTask(int $userId)
+    {
+        $task = new Task();
+        $task->setUser($this->createUser($userId));
+
+        return $task;
+    }
+}
